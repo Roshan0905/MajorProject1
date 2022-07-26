970247c56f123253ea9a44e6dae2701df5b51a94
Roshan0905 <98831099+Roshan0905@users.noreply.github.com>
2022-07-27 02:04:46 +0530

    views

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 //use express router
 app.use('/', require('./routes'));
 
+//set up the view engines
+app.set('view engine', 'views');
+app.set('views', './views');
+
 
 app.listen(port, function(err){
     if (err){
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ console.log("router loaded");
 
 
 router.get('/', homeContoller.home );
+router.use('/users', require('./users'));
+
+//for any further routes, access from here
+//router.use('/routerName', require('./routerfile'));
+
+
 module.exports = router;
