db4f56b08c8c41a1d8ef629081f586b152e82563
Roshan0905 <98831099+Roshan0905@users.noreply.github.com>
2022-08-10 21:55:33 +0530

    Authentication Using Passport js

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
@@ -11,5 +12,10 @@ router.get('/sign-in', usersController.signIn);
 
 router.post('/create', usersController.create);
 
+//use passport as a middleware to authenticate
+router.post('/create-session',passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'},
+) , usersController.createSession);
 
 module.exports = router;
